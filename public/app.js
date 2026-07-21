618374cee3b4b8d02b2f2c88b21a2115f976d889
Pedro <pedro@local>
2026-07-21 18:55:07 -0300

    Trocar nome de exemplo no formulario de novo usuario

--- public/app.js
+++ public/app.js
@@ -1740,7 +1740,7 @@ function usersTemplate(){
       <div class="form-row">
         <div>
           <label for="newUserNome">Nome</label>
-          <input type="text" id="newUserNome" placeholder="Ex: Ana Souza" required>
+          <input type="text" id="newUserNome" placeholder="Ex: Larissa Martins" required>
         </div>
         <div>
           <label for="newUserRole">Tipo de conta</label>
